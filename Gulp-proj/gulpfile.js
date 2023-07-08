140b989bb35fbdf91da969cf3884c7e0bed1528d
alexua99 <2356931k@gmail.com>
2023-07-08 13:09:02 +0300

    test

--- Gulp-proj/gulpfile.js
+++ Gulp-proj/gulpfile.js
@@ -1,20 +1,33 @@
 const {src, dest, watch, parallel} = require('gulp') 
 const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
 
+
+const option = {
+    out: './Dist',
+    src: './src'
+}
 
 // Style 
 function style(){
-    return src('./src/style/main.scss')
+    return src(`${option.src}/style/main.scss`)
+    .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
-    .pipe(dest('./dist'))
+    .pipe(sourcemaps.write())
+    .pipe(dest(`${option.out}`))
 
 }
+
+// Html 
+ function htmlTest() {
+    return src(`${option.src}/**/*.html`).pipe(dest(`${option.out}`))
+ }
 
 
 //Watch
 
 function watchAll(){
-    watch('./src/style/main.scss', style)
+    watch(`${option.src}/style/**/*.scss`, style)
 }
 
-exports.default = parallel(style, watchAll)
+exports.default = parallel(htmlTest, style, watchAll)
